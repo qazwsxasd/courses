7bad63916ca47f5c5687b084c7b561ba24561ed0
Olga Melnichuk <Olga.Melnichuk@epam.com>
2018-02-01 14:49:56 +0200

    edit page

--- course-app/src/app/modules/edit/edit.component.ts
+++ course-app/src/app/modules/edit/edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { Course } from '../../core/models/course.model';
 
 @Component({
   selector: 'app-course-edit',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
+  private course: Course;
+  fg: FormGroup;
 
-  constructor() { }
+  constructor(
+    private builder: FormBuilder // reactive forms
+  ) { }
 
   ngOnInit() {
+    // this.course = this.builder.group();
+    // this.fg = new FormGroup({ // long way
+    //   course: new FormGroup({
+    //     title: new FormControl(''),
+    //     description: new FormControl('')
+    //   })
+    // });
+
+    this.fg = this.builder.group({
+      courseName: this.builder.group({
+        titleName: ''
+      })
+    });
+
+    // this.fg.setValue({
+    //   courseName: {
+    //     titleName: ''
+    //   }
+    // });
   }
 
 }
